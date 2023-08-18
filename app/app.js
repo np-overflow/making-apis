15bd0eb1aa18a3bcd427cb0748e84d7dc7f6d080
Arash <47273556+arashnrim@users.noreply.github.com>
2023-08-18 06:49:02 +0000

    Change arrow function to longhand method

--- app/app.js
+++ app/app.js
@@ -3,12 +3,11 @@ const app = express();
 const PORT = 3000;
 
 // Creates a GET endpoint at the root URL that returns a string
-// () => {} is the alternative to function() {}; it's called an arrow function!
-app.get("/", (request, response) => {
+app.get("/", function (request, response) {
     response.send("Hello, world!");
 });
 
 // Starts the server on port 3000
-app.listen(PORT, () => {
+app.listen(PORT, function() {
     console.log(`Server listening on port ${PORT}!`);
 });
